ba43d150fdbc1749d08a705d9fac026b32146522
Sanique1234 <73706202+Sanique1234@users.noreply.github.com>
2022-05-05 13:05:44 -0500

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <META CHARSET="UTF-8">
+            <TITLE>Home</TITLE>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    </head>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style="background-color:blue;">
+    <div class="navbar-nav">
+      <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <a class="nav-link active" href="Registration.php">Registration</a>
+      <a class="nav-link active" href="Courses.php">Courses</a>
+      <a class="nav-link active" href="Payment.php">Payment</a>
+      <a class="nav-link active" href="Aboutus.php">About Us</a>
+      <a class="nav-link active" href="FAQ.php">FAQ</a>
+    </div>
+  </div>
+</div>
+</nav>
+<div class="v-item-group theme--light v-expansion-panels v-expansion-panels--popout">
+<div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What is the Pearson MyFoundations Lab for Freshman Composition?</button>
+<div class="v-expansion-panel-content ml-4 mb-4 text-left" style="display: none;">
+<div class="v-expansion-panel-content__wrap">
+<div class="row"><div class="col col-12"> It is an online study tool that provides supplementary content for Freshman Composition I &amp; II and allows for self-paced learning. <br><br><!----><!----></div></div><br><hr role="separator" aria-orientation="horizontal" class="v-divider theme--light"><br><div class="row"><div class="col col-12"><div class="adsbygoogle adswrapper" style="display: block;"><!---->  <ins data-ad-format="fluid" data-ad-layout-key="-fb+5w+4e-db+86" data-ad-client="ca-pub-7094136257269150" data-ad-slot="1088191821" data-ad-test="" data-ad-region="" class="adsbygoogle" style="display: block;" data-adsbygoogle-status="done"></ins> <!----> <script type="text/javascript"> (adsbygoogle = window.adsbygoogle || []).push({}); </script></div></div></div></div></div></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What is an access code?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What is a course ID?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What is the Path Builder?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">Who should do the Pearson MyFoundations Lab?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">How do I register for the Pearson MyFoundations Lab?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">If I created a Pearson account in Freshman Composition I, do I need to create another account in Freshman Composition II?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">How should I do the Labs?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What if I enrol in the wrong course or section?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">How do I know I have successfully completed the Labs?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">Where do I find the access code?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What should I do if I do not see the access code on Aeorion?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What should I do if my access code is invalid?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What should I do if I am having issues while registering?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">Can I use my personal email account to register for the Pearson MyFoundations Lab?</button><!----></div><div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What is the due date for the Lab?</button><!----></div></div>
+<div aria-expanded="false" class="v-expansion-panel"><button type="button" class="v-expansion-panel-header">What is a course ID?</button><!----></div>
+</html>
